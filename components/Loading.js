dd21410e31f7a1f755fa5cc9d1aaddbc04636d72
mert-donmez <83416622+mert-donmez@users.noreply.github.com>
2023-04-16 08:50:55 +0300

    Add files via upload

---
+++ components/Loading.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View,Image } from 'react-native'
+import React from 'react'
+import './loading.gif'
+
+const Loading = () => {
+  return (
+    <View style={{justifyContent:'center',alignItems:'center'}}>
+      <Image source={require('./loading.gif')} style={{width:260,height:260}}/>
+      </View>
+  )
+}
+
+
+
+export default Loading;
